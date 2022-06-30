1c1415eed979a5f62c33d393ef5b6223d97ca1d9
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-30 19:31:30 +0200

    Adding register user endpoint

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,11 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const { validate } = require("express-validation");
-const { loginUser } = require("../server/controllers/userControllers");
-const credentialsLoginSchema = require("../utils/schemas/userCredentialsSchema");
+const {
+  loginUser,
+  registerUser,
+} = require("../server/controllers/userControllers");
+const {
+  credentialsLoginSchema,
+  credentialsRegisterSchema,
+} = require("../utils/schemas/userCredentialsSchema");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/login", validate(credentialsLoginSchema), loginUser);
+
+usersRouter.post(
+  "/register",
+  validate(credentialsRegisterSchema),
+  registerUser
+);
 
 module.exports = usersRouter;
